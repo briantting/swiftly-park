661f6e6463bf5599b00f84bda5f1e46a384781c4
Brendon Lavernia <brendonlavernia@gmail.com>
2016-04-30 00:51:01 -0400

    Wrote functions to convert HTTP request to nodes

--- HTTPServer/app.swift
+++ HTTPServer/app.swift
@@ -24,6 +24,7 @@ let port = 3000
 // ---- [ imports ] -----------------------------------------------------------
 
 import Darwin
+import MapKit
 
 // ---- [ includes ] ----------------------------------------------------------
 
@@ -69,6 +70,249 @@ extension String {
             }
         }
         return -1
+    }
+}
+
+// ---- [ ParkingSpot ] --------------------------------------------------------
+
+class ParkingSpot: MKPointAnnotation {
+    let lat: Double
+    let long: Double
+    
+    init(_ coordinate: CLLocationCoordinate2D) {
+        self.long = coordinate.latitude
+        self.lat = coordinate.longitude
+        super.init()
+        self.coordinate = coordinate
+    }
+}
+
+class longSpot: ParkingSpot, Comparable {}
+class latSpot: ParkingSpot, Comparable {
+    func asLongSpot() -> longSpot {
+        return longSpot(self.coordinate)
+    }
+}
+
+func < (left: longSpot, right: longSpot) -> Bool {
+    return left.lat < right.lat
+}
+
+func < (left: latSpot, right: latSpot) -> Bool {
+    return left.long < right.long
+}
+
+func getSpots(spotsByLat: Node<longSpot>,
+              spotsByLong: Node<latSpot>,
+              upperLeft: CLLocationCoordinate2D,
+              lowerRight: CLLocationCoordinate2D) -> Set<ParkingSpot> {
+    
+    let upperLeftSpot = ParkingSpot(upperLeft)
+    let lowerRightSpot = ParkingSpot(lowerRight)
+    let spotsInXRange = spotsByLat.valuesBetween(upperLeftSpot as! longSpot,
+                                                 and: lowerRightSpot as! longSpot)
+    return spotsByLong.valuesBetween(upperLeftSpot as! latSpot,
+                                     and: lowerRightSpot as! latSpot,
+                                     if: {spotsInXRange.contains($0.asLongSpot())})
+}
+
+
+// ---- [ AVL Binary Tree ] --------------------------------------------------------
+
+indirect enum Node<T where T:Comparable, T:Hashable> : CustomStringConvertible {
+    case Leaf
+    case Tree(Node<T>, T, Node<T>, Int)
+    
+    var description: String {
+        switch self {
+        case .Leaf:
+            return "_"
+        case let Tree(left, v, right, h):
+            return "(\(left) \(v)[\(h)] \(right))"
+        }
+    }
+    
+    func value() -> T? {
+        switch self {
+        case let Tree(_, root, _, _): return root
+        case .Leaf: return nil
+        }
+    }
+    
+    func getLeft() -> Node<T>? {
+        switch self {
+        case let Tree(l, _, _, _): return l
+        case .Leaf: return nil
+        }
+    }
+    
+    func getRight() -> Node<T>? {
+        switch self {
+        case let Tree(_, _, r, _): return r
+        case .Leaf: return nil
+        }
+    }
+    
+    func height() -> Int {
+        switch self {
+        case Tree(_, _, _, let height): return height
+        case Leaf: return 0
+        }
+    }
+    
+    func getHeight(left: Node<T>, _ right: Node<T>) -> Int {
+        return max(left.height(), right.height()) + 1
+    }
+    
+    func apply<U>(f: Node<T> -> U,
+               onLeftBranchIf condition: Bool) -> U? {
+        if case let Tree(left, _, right, _) = self {
+            return condition ? f(left) : f(right)
+        }
+        return nil
+    }
+    
+    func substitute(f: Node<T> -> Node<T>,
+                    forLeftBranchIf condition: Bool) -> Node<T>? {
+        if case let Tree(left, root, right, _) = self {
+            var (left, right) = (left, right)
+            if condition {
+                left = f(left)
+            } else {
+                right = f(right)
+            }
+            return Tree(left, root, right, getHeight(left, right))
+        }
+        return nil
+    }
+    
+    func insert(value: T) -> Node<T> {
+        switch self {
+        case let Tree(_, root, _, _):
+            return self
+                .substitute({$0.insert(value)},
+                            forLeftBranchIf: value < root)!
+                .rotate()
+            
+        case Leaf:
+            return Tree(Node.Leaf, value, Node.Leaf, 1)
+        }
+    }
+    
+    
+    func far(left left: Bool, prev: Node<T>? = nil) -> T? {
+        switch self {
+        case Tree:
+            return apply({$0.far(left: left, prev: self)},
+                         onLeftBranchIf: left)!
+        case Leaf:
+            if prev != nil {
+                if case let Tree(_, value, _, _) = prev! {
+                    return value
+                }
+            }
+        }
+        return nil
+    }
+    
+    func removeFar(left left: Bool) -> (Node<T>)? {
+        
+        switch self {
+        case Leaf: return nil
+        case let Tree(leftBranch, _, rightBranch, _):
+            
+            func recursion(branch: Node<T>) -> Node<T> {
+                switch branch {
+                case Tree: return self
+                    .substitute({$0.removeFar(left: left)!},
+                                forLeftBranchIf: left)!
+                case Leaf:
+                    return left ? rightBranch : leftBranch
+                }
+            }
+            
+            return self
+                .apply(recursion, onLeftBranchIf: left)!
+                .rotate()
+        }
+    }
+    
+    func remove(value: T) -> Node<T> {
+        switch self {
+        case let Tree(left, root, right, _):
+            if value == root {
+                switch right {
+                case Tree:
+                    let newRoot = right.far(left: true)!
+                    let newRight = right.removeFar(left: true)!
+                    let newHeight = getHeight(left, newRight)
+                    return Tree(left, newRoot, newRight, newHeight)
+                        .rotate()
+                case Leaf: return left
+                }
+            } else {
+                return self.substitute({$0.remove(value)},
+                                       forLeftBranchIf: value < root)!
+                    .rotate()
+            }
+        case Leaf:
+            return self;
+        }
+    }
+    
+    func rotate() -> Node<T> {
+        switch self {
+        case Leaf: return Leaf
+        case let Tree(leftBranch, _, rightBranch, _):
+            switch leftBranch.height() - rightBranch.height() {
+            case -1...1:
+                return self
+            default:
+                let leftIsHigher = leftBranch.height() > rightBranch.height()
+                let branch = leftIsHigher ? leftBranch : rightBranch
+                switch branch {
+                case Leaf: return self
+                case let Tree:
+                    
+                    func graft(middleBranch: Node<T>) -> Node<T> {
+                        return self.substitute({_ in middleBranch},
+                                               forLeftBranchIf: leftIsHigher)!
+                    }
+                    
+                    return branch.substitute({graft($0)},
+                                             forLeftBranchIf: !leftIsHigher)!
+                }
+            }
+        }
+    }
+    
+    func valuesBetween(a: T, and b: T,
+                       if condition: T->Bool = {_ in true}) -> Set<T> {
+        switch self {
+        case .Leaf:
+            return Set()
+        case let Tree(left, root, right, _):
+            switch root {
+            case a...b:
+                let values = [left, right]
+                    .map({$0.valuesBetween(a, and: b, if: condition)})
+                let s = condition(root) ? Set([root]) : Set()
+                return s.union(values[0])
+                    .union(values[1])
+            default:
+                return self.apply({$0.valuesBetween(a, and: b, if: condition)},
+                                  onLeftBranchIf: a < root)!
+            }
+        }
+    }
+    
+    func balanced() -> Bool {
+        switch self {
+        case Leaf: return true
+        case let Tree(leftBranch, _, rightBranch, _):
+            return abs(leftBranch.height() - rightBranch.height()) < 2
+                && leftBranch.balanced() && rightBranch.balanced()
+        }
     }
 }
 
@@ -228,6 +472,7 @@ class Socket {
  * File: http.swift
  * Desc: Various HTTP constructs.
  * Auth: Cezary Wojcik
+ * Modified: Brendon Lavernia
  */
 
 // ---- [ includes ] ----------------------------------------------------------
@@ -239,6 +484,9 @@ struct HTTPRequest {
     let cs : ClientSocket
     let raw : String
     let rawHeaders : [String : String]
+    let commandMsg : String
+    let isGetCommand : Bool
+    let isInvalidRequest : Bool
 
     // ---- [ setup ] ---------------------------------------------------------
 
@@ -250,6 +498,13 @@ struct HTTPRequest {
         // get request data
         self.cs = cs
         let lines = cs.fetchRequest()
+        
+        //Get command and message
+        let commandIndex = lines[0].indexOf(" ")
+        let command = lines[0][0..<commandIndex]
+        let msgIndex = lines[0][commandIndex+1..<lines[0].characters.count].indexOf(" ")
+        commandMsg = lines[0][commandIndex + 1..<msgIndex]
+        
 
         // parse request headers
         for i in 1..<lines.count {
@@ -262,10 +517,22 @@ struct HTTPRequest {
             tempRawHeaders[headerName] = headerContent
             tempRaw += lines[i]
         }
+        
+        if command == "GET" {
+            isGetCommand = true
+            isInvalidRequest = false
+        } else if command == "POST" {
+            isGetCommand = false
+            isInvalidRequest = false
+        } else {
+            isGetCommand = false
+            isInvalidRequest = true
+        }
 
         // assign temp vars to the struct members
         raw = tempRaw
         rawHeaders = tempRawHeaders
+        
     }
 
     // ---- [ instance methods ] ----------------------------------------------
@@ -273,6 +540,7 @@ struct HTTPRequest {
     func clientAddress() -> String? {
         return cs.clientAddress()
     }
+    
 }
 
 struct HTTPResponse {
@@ -319,9 +587,65 @@ class Server {
     }
 }
 
+
+// ---- [ Process Get Command ] ------------------------------------------------------
+func processGetCommand(msg : String, _ latTree : Node<latSpot>, _ longTree : Node<longSpot>) -> String {
+    let coordinates = convertStringToSpots(msg)
+    let northWest = coordinates[0]
+    let southEast = coordinates[1]
+    let spotsWithinMap = getSpots(longTree, spotsByLong: latTree, upperLeft: northWest, lowerRight: southEast)
+    return convertSpotsToString(spotsWithinMap)
+}
+
+// ---- [ Process Post Command ] ------------------------------------------------------
+func processPostCommand(msg : String, _ latTree : Node<latSpot>, _ longTree : Node<longSpot>) -> Void {
+    let coordinates = convertStringToSpots(msg)
+    for coordinate in coordinates {
+        latTree.insert(latSpot(coordinate))
+        longTree.insert(longSpot(coordinate))
+    }
+}
+
+func convertSpotsToString(spots : Set<ParkingSpot>) -> String {
+    var stringSpots = ""
+    for spot in spots {
+        stringSpots += String(spot.lat)
+        stringSpots += ","
+        stringSpots += String(spot.long)
+    }
+    return stringSpots
+}
+
+func convertStringToSpots(msg : String) -> [CLLocationCoordinate2D] {
+    var spots = [CLLocationCoordinate2D]()
+    var spotList = msg
+    while spotList.characters.count > 0 {
+        let firstIndex = spotList.indexOf(",")
+        
+        var secondIndex = spotList[firstIndex+1..<spotList.characters.count].indexOf(",")
+        if secondIndex == -1  {
+            secondIndex = spotList.characters.count
+        }
+        let firstCoordinate = Double(spotList[0..<firstIndex])
+        let secondCoordinate = Double(spotList[firstIndex + 1..<secondIndex])
+        
+        spots.append(CLLocationCoordinate2D(latitude: firstCoordinate!, longitude: secondCoordinate!))
+        guard secondIndex != spotList.characters.count else {
+            break
+        }
+        spotList = spotList[secondIndex + 1..<spotList.characters.count]
+        
+    }
+    
+    return spots
+}
+
+
 // ---- [ server setup ] ------------------------------------------------------
 
 let app = Server(port: port)
+let spotsByLat = Node<latSpot>.Leaf
+let spotsByLong = Node<longSpot>.Leaf
 
 print("Running server on port \(port)")
 
@@ -336,6 +660,18 @@ app.run() {
 
     // print request headers
     print(request.rawHeaders)
+    
+    let responseMsg : String
+    
+    if request.isInvalidRequest {
+        responseMsg = "Invalid request"
+    }
+    else if request.isGetCommand {
+        responseMsg = processGetCommand(request.commandMsg, spotsByLat, spotsByLong)
+    } else {
+        processPostCommand(request.commandMsg, spotsByLat, spotsByLong)
+        responseMsg = "Post successful"
+    }
     response.sendRaw("HTTP/1.1 200 OK\n\nHello, World!\n")
 }
 
