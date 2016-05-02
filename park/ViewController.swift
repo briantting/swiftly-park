e20528abf72881486046ff10052e71de09546006
Brian Ting <brian.t.ting@gmail.com>
2016-05-02 12:13:09 -0400

    fixed mapview thang

--- park/ViewController.swift
+++ park/ViewController.swift
@@ -84,7 +84,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         mapView.removeAnnotations(mapView.annotations.filter()
             {$0 !== mapView.userLocation})
         // adds new parking spots
-        let (upperLeft, lowerRight) = getMapBounds()
+        let (upperLeft, lowerRight) = mapView.getMapBounds()
         let parkingSpots = server.getParkingSpots(upperLeft, lowerRight)
 
         mapView.showsUserLocation = true
