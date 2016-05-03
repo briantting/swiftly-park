3538c2284404f3141bc9e2280bc200deb5067b36
Ethan <ethanabrooks@gmail.com>
2016-05-03 16:11:09 -0400

    merged. and everything runs, I think

--- Model.swift
+++ Model.swift
@@ -13,15 +13,9 @@ import MapKit
  A protocol (interface) for handling requests for getting parking spots and updating parking spots
  */
 protocol Model {
-<<<<<<< HEAD
-    func getSpots(upperLeft: CLLocationCoordinate2D,
-       _ lowerRight: CLLocationCoordinate2D) -> Set<ParkingSpot>
-    mutating func addSpot(coordinate: CLLocationCoordinate2D)
-=======
     func spotsWithinView(upperLeft: CLLocationCoordinate2D,
                          _ lowerRight: CLLocationCoordinate2D) -> Set<ParkingSpot>
     mutating func addSpot(coordinate: CLLocationCoordinate2D)
     mutating func removeSpotNear(coordinate: CLLocationCoordinate2D,
                                  radius: Double)
->>>>>>> 31925fbf7e5537802f700fcc384d32132947a02f
 }
